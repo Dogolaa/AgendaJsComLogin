2efbc622642ddc1430c694d40efd00c2f55eb126
Dogolaa <lucasdogo01@gmail.com>
2025-01-29 19:05:58 -0300

    bug: Removendo o helmet para que o site funcione corretamente

--- server.js
+++ server.js
@@ -24,6 +24,8 @@ const {
   csrfMiddleware,
 } = require('./src/middlewares/middleware');
 
+// app.use(helmet());
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(path.resolve(__dirname, 'public')));
